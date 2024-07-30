87e628d74e319f2abdc3efe08f8f558888022fe8
Better-Great <abiegbeg@gmail.com>
2024-07-30 10:53:18 +0100

    Creating vpc and firewall using terraform

---
+++ infrastructure/tags.tf
@@ -0,0 +1,7 @@
+resource "digitalocean_tag" "socks_shop" {
+  name = "socks-shop"
+}
+
+resource "digitalocean_tag" "development" {
+  name = "development"
+}
---
+++ infrastructure/variable.tf
@@ -0,0 +1,40 @@
+variable "do_token" {
+  description = "DigitalOcean API token"
+  type        = string
+  sensitive   = true
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "region" {
+  description = "DigitalOcean region"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_description" {
+  description = "Description of the VPC"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "Name of the firewall"
+  type        = string
+}
+
+# variable "registry_name" {
+#   description = "Name of the container registry"
+#   type        = string
+# }
+
+# variable "subscription_tier" {
+#   description = "Subscription tier for the container registry"
+#   type        = string
+# }
